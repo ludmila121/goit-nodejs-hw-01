b50f8e732d58dbe8e0a9ef109561552f443382ca
ludmila121 <113711608+ludmila121@users.noreply.github.com>
2023-07-05 13:00:33 +0200

    catch

--- contacts.js
+++ contacts.js
@@ -8,40 +8,57 @@ await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 }
 // TODO: задокументувати кожну функцію
   async function listContacts() {
-   const contacts = await fs.readFile(contactsPath);
-   return JSON.parse(contacts);
+    try{
+      const contacts = await fs.readFile(contactsPath);
+      return JSON.parse(contacts);
+} catch (error){
+  console.log(error.message);
+}
 } 
  async function getContactById(contactId) {
+  try{
     const contacts = await listContacts();
     const result = contacts.find(({id}) => id === contactId);
     if(!result) {
         return null;
-    }
-    return result;
-  } 
+    }  return result;
+  } catch (error) { 
+    console.log(error.message);
+  }}
+
   
    async function removeContact(contactId) {
-    const contacts = await listContacts();
-    const idx = contacts.findIndex(({id}) => id === contactId);
-    if (idx === -1) {
-        return null;
+    try{
+      const contacts = await listContacts();
+      const idx = contacts.findIndex(({id}) => id === contactId);
+      if (idx === -1) {
+          return null;
+      }
+      const [removeContact] = contacts.splice(idx,1);
+      updateContacts(contacts);
+      return removeContact;
+    } catch (error){
+      console.log(error.message);
     }
-    const [removeContact] = contacts.splice(idx,1);
-    updateContacts(contacts);
-    return removeContact;
   } 
+
   
-  async function addContact(name, email, phone) {
-    const contacts = await listContacts();
-    const newContact = {
-       id: nanoid(),
-       name,
-       email,
-       phone, 
-    };
-    contacts.push(newContact);
-    updateContacts(contacts);
-    return newContact;
+  async function addContact(name, email, phone) { 
+    try {
+      const contacts = await listContacts();
+      const newContact = {
+         id: nanoid(),
+         name,
+         email,
+         phone, 
+      };
+      contacts.push(newContact);
+      updateContacts(contacts);
+      return newContact;
+    } catch (error){
+      console.log(error.message);
+    }
+
   } 
   module.exports = {
     listContacts,
